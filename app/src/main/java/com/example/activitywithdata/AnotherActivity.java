14b315e401981b56f434ab5b188f8b8f259a1c23
Dawn-deer <2236495861@qq.com>
2020-11-03 19:30:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/activitywithdata/AnotherActivity.java
@@ -0,0 +1,34 @@
+package com.example.activitywithdata;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+
+        Button button2 = (Button) findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = getIntent();
+                String name = intent.getStringExtra("name");
+                Integer age = intent.getIntExtra("age", 20);
+                Toast.makeText(AnotherActivity.this,name + " " + age, Toast.LENGTH_LONG).show();
+                intent.putExtra("result", "姓名:" + name + " 年龄" + age);
+                setResult(0, intent);
+                finish();
+            }
+        });
+    }
+
+}
